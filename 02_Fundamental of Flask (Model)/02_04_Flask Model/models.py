448192156a1ab4b1c131a9f1c96f776480d0ff84
Isman Subarkah <ismansubarkah.2210@gmail.com>
2022-04-25 10:12:19 +0700

    Add new Models File

---
+++ 02_Fundamental of Flask (Model)/02_04_Flask Model/models.py
@@ -0,0 +1,11 @@
+class Hello(object):
+    def __init__(self):
+        self.data = 0
+
+    def getData(self):
+        return self.data
+        
+    def setData(self, d):
+        self.data = d
+        
+        
