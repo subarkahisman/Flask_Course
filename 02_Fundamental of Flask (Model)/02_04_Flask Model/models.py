cc5b7f196599bcdc8bf642439b5795157cd4a559
Isman Subarkah <ismansubarkah.2210@gmail.com>
2022-04-26 08:34:59 +0700

    Modifiying code

--- 02_Fundamental of Flask (Model)/02_04_Flask Model/models.py
+++ 02_Fundamental of Flask (Model)/02_04_Flask Model/models.py
@@ -1,11 +1,17 @@
 class Hello(object):
     def __init__(self):
         self.data = 0
-
+        self.value = 0
+        
     def getData(self):
         return self.data
         
     def setData(self, d):
         self.data = d
         
+    def getValue(self):
+        return self.value
+        
+    def setValue(self, v):
+        self.value = v
         
