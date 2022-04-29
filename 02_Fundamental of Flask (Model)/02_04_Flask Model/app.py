844b34bc9450e967280eb4a4e8a38f5e420f0bf0
Isman Subarkah <ismansubarkah.2210@gmail.com>
2022-04-29 09:23:09 +0700

    Modifying code again although today is holiday

--- 02_Fundamental of Flask (Model)/02_04_Flask Model/app.py
+++ 02_Fundamental of Flask (Model)/02_04_Flask Model/app.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template
+from models import User
 
 app = Flask(__name__)
 
-app.route('/')
-def home():
-    model = Model()
-    render_template(model=model)
+app.route('/login', methods=['GET', 'POST'])
+def index():
+    model = User()
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        model.setUsername(username)
+        model.setPassword(password)
+        if model.authenticate(username):
+            return render_template('login_sukses.html', model=model)
+        else:
+            return render_template('login_error.html')
+    
+    else:    
+        render_template('login_form.html', model=model)
     
 if __name__ == '__main__':
     app.run(debug=True)
